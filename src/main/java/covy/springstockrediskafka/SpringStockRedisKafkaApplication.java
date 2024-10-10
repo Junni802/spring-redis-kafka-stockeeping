3c5278551ec48ae9fd9d22ae404e862f8e0fdfa9
박준혁/PD프로젝트 <junni802@gsretail.com>
2024-10-10 18:21:36 +0900

    first commit

---
+++ src/main/java/covy/springstockrediskafka/SpringStockRedisKafkaApplication.java
@@ -0,0 +1,13 @@
+package covy.springstockrediskafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStockRedisKafkaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringStockRedisKafkaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/covy/springstockrediskafka/SpringStockRedisKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package covy.springstockrediskafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStockRedisKafkaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
